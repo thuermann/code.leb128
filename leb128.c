6e0d256ac15c006fe3ac02769f2073534ccf1f03
Urs Thuermann <urs@isnogud.escape.de>
2018-09-24 23:01:48 +0200

    Use long long and unsigned long long for integers to be {en,de}coded
    Use typedefs 'signed_int' and 'unsigned_int' to allow easier change
    of the actual type used for encoded and decoded integers.

--- leb128.c
+++ leb128.c
@@ -1,5 +1,5 @@
 /*
- * $Id: leb128.c,v 1.3 2018/09/24 21:00:43 urs Exp $
+ * $Id: leb128.c,v 1.4 2018/09/24 21:01:48 urs Exp $
  */
 
 #include <stdlib.h>
@@ -12,10 +12,13 @@ static void usage(const char *name)
     fprintf(stderr, "Usage: %s [-u] [-e|-d] code\n", name);
 }
 
-static void unsigned_long_to_leb128(unsigned long val, unsigned char *s);
-static void signed_long_to_leb128(long val, unsigned char *s);
-static unsigned long unsigned_leb128_to_long(const unsigned char *s);
-static long signed_leb128_to_long(const unsigned char *s);
+typedef long long signed_int;
+typedef unsigned long long unsigned_int;
+
+static void unsigned_int_to_leb128(unsigned_int val, unsigned char *s);
+static void signed_int_to_leb128(signed_int val, unsigned char *s);
+static unsigned_int unsigned_leb128_to_int(const unsigned char *s);
+static signed_int signed_leb128_to_int(const unsigned char *s);
 
 static void print_leb128(const unsigned char *leb128);
 static void read_leb128(int argc, char **argv, unsigned char *leb128);
@@ -49,32 +52,32 @@ int main(int argc, char **argv)
 
     if (mode == ENCODE && opt_unsigned) {
 	unsigned char leb128[16];
-	unsigned long val = strtoul(argv[optind], NULL, 0);
-	unsigned_long_to_leb128(val, leb128);
+	unsigned_int val = strtoull(argv[optind], NULL, 0);
+	unsigned_int_to_leb128(val, leb128);
 	print_leb128(leb128);
     } else if (mode == ENCODE && !opt_unsigned) {
 	unsigned char leb128[16];
-	long val = strtol(argv[optind], NULL, 0);
-	signed_long_to_leb128(val, leb128);
+	signed_int val = strtoll(argv[optind], NULL, 0);
+	signed_int_to_leb128(val, leb128);
 	print_leb128(leb128);
     } else if (mode == DECODE && opt_unsigned) {
 	unsigned char leb128[16];
-	unsigned long val;
+	unsigned_int val;
 	read_leb128(argc - optind, argv + optind, leb128);
-	val = unsigned_leb128_to_long(leb128);
-	printf("%lx\n", val);
+	val = unsigned_leb128_to_int(leb128);
+	printf("%llx\n", val);
     } else if (mode == DECODE && !opt_unsigned) {
 	unsigned char leb128[16];
-	long val;
+	signed_int val;
 	read_leb128(argc - optind, argv + optind, leb128);
-	val = signed_leb128_to_long(leb128);
-	printf("%lx\n", val);
+	val = signed_leb128_to_int(leb128);
+	printf("%llx\n", val);
     }
 
     return 0;
 }
 
-static void unsigned_long_to_leb128(unsigned long val, unsigned char *s)
+static void unsigned_int_to_leb128(unsigned_int val, unsigned char *s)
 {
     unsigned char c;
     int more;
@@ -84,13 +87,13 @@ static void unsigned_long_to_leb128(unsigned long val, unsigned char *s)
 	val >>= 7;
 	more = val != 0;
 #ifdef DEBUG
-	fprintf(stderr, "%.2x %lx\n", c, val);
+	fprintf(stderr, "%.2x %llx\n", c, val);
 #endif
 	*s++ = c | (more ? 0x80 : 0);
     } while (more);
 }
 
-static void signed_long_to_leb128(long val, unsigned char *s)
+static void signed_int_to_leb128(signed_int val, unsigned char *s)
 {
     unsigned char c;
     int more;
@@ -100,22 +103,22 @@ static void signed_long_to_leb128(long val, unsigned char *s)
 	val >>= 7;
 	more = c & 0x40 ? val != -1 : val != 0;
 #ifdef DEBUG
-	fprintf(stderr, "%.2x %lx\n", c, val);
+	fprintf(stderr, "%.2x %llx\n", c, val);
 #endif
 	*s++ = c | (more ? 0x80 : 0);
     } while (more);
 }
 
-static unsigned long unsigned_leb128_to_long(const unsigned char *s)
+static unsigned_int unsigned_leb128_to_int(const unsigned char *s)
 {
-    unsigned long c, val = 0;
+    unsigned_int c, val = 0;
     int shift = 0;
     int more;
 
     do {
 	c = *s++;
 #ifdef DEBUG
-	fprintf(stderr, "%.2lx %lx\n", c, val);
+	fprintf(stderr, "%.2llx %llx\n", c, val);
 #endif
 	val |= (c & 0x7f) << shift;
 	shift += 7;
@@ -125,23 +128,23 @@ static unsigned long unsigned_leb128_to_long(const unsigned char *s)
     return val;
 }
 
-static long signed_leb128_to_long(const unsigned char *s)
+static signed_int signed_leb128_to_int(const unsigned char *s)
 {
-    unsigned long c, val = 0;
+    unsigned_int c, val = 0;
     int shift = 0;
     int more;
 
     do {
 	c = *s++;
 #ifdef DEBUG
-	fprintf(stderr, "%.2lx %lx\n", c, val);
+	fprintf(stderr, "%.2llx %llx\n", c, val);
 #endif
 	val |= (c & 0x7f) << shift;
 	shift += 7;
 	more = c & 0x80;
     } while (more);
 
-    if (c & 0x40 && shift < sizeof(unsigned long) * CHAR_BIT)
+    if (c & 0x40 && shift < sizeof(unsigned_int) * CHAR_BIT)
 	val |= ~0UL << shift;
 
     return val;
