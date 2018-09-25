1560c154bf83f80f7169ad4ae366ada5b4084388
Urs Thuermann <urs@isnogud.escape.de>
2018-09-26 01:54:17 +0200

    Add zigzag encoding
    Add option -z to add zigzag encoding before unsigned LEB128 encoding
    and zigzag decoding after unsigned LEB128 decoding.

--- leb128.c
+++ leb128.c
@@ -1,5 +1,5 @@
 /*
- * $Id: leb128.c,v 1.5 2018/09/25 23:54:08 urs Exp $
+ * $Id: leb128.c,v 1.6 2018/09/25 23:54:17 urs Exp $
  */
 
 #include <stdlib.h>
@@ -9,13 +9,16 @@
 
 static void usage(const char *name)
 {
-    fprintf(stderr, "Usage: %s [-u] [-e|-d] code\n", name);
+    fprintf(stderr, "Usage: %s [-u|-z] [-e|-d] code\n", name);
 }
 
 typedef long long signed_int;
 typedef unsigned long long unsigned_int;
 
 static const int uint_bits = sizeof(unsigned_int) * CHAR_BIT;
+
+static unsigned_int zigzag(signed_int n);
+static signed_int de_zigzag(unsigned_int n);
 
 static void unsigned_int_to_leb128(unsigned_int val, unsigned char *s);
 static void signed_int_to_leb128(signed_int val, unsigned char *s);
@@ -28,13 +31,16 @@ static void read_leb128(int argc, char **argv, unsigned char *leb128);
 int main(int argc, char **argv)
 {
     enum { ENCODE, DECODE } mode = ENCODE;
-    int errflag = 0, opt_unsigned = 0;
+    int errflag = 0, opt_unsigned = 0, opt_zigzag = 0;
     int opt;
 
-    while ((opt = getopt(argc, argv, "ued")) != -1) {
+    while ((opt = getopt(argc, argv, "uzed")) != -1) {
 	switch (opt) {
 	case 'u':
 	    opt_unsigned = 1;
+	    break;
+	case 'z':
+	    opt_zigzag = 1;
 	    break;
 	case 'e':
 	    mode = ENCODE;
@@ -60,7 +66,10 @@ int main(int argc, char **argv)
     } else if (mode == ENCODE && !opt_unsigned) {
 	unsigned char leb128[16];
 	signed_int val = strtoll(argv[optind], NULL, 0);
-	signed_int_to_leb128(val, leb128);
+	if (opt_zigzag)
+	    unsigned_int_to_leb128(zigzag(val), leb128);
+	else
+	    signed_int_to_leb128(val, leb128);
 	print_leb128(leb128);
     } else if (mode == DECODE && opt_unsigned) {
 	unsigned char leb128[16];
@@ -72,11 +81,43 @@ int main(int argc, char **argv)
 	unsigned char leb128[16];
 	signed_int val;
 	read_leb128(argc - optind, argv + optind, leb128);
-	val = signed_leb128_to_int(leb128);
+	if (opt_zigzag)
+	    val = de_zigzag(unsigned_leb128_to_int(leb128));
+	else
+	    val = signed_leb128_to_int(leb128);
 	printf("%llx\n", val);
     }
 
     return 0;
+}
+
+/* Google Protobuf encodes signed integers using zigzag encoding and then
+ * encoding the result as unsigned LEB128.  Zigzag encoding ensures that
+ * signed numbers with small absolute values are mapped to small unsigned
+ * numbers.  Non-negative number are simply doubled while for negative
+ * numbers the absolute values is doubled and then decremented by one.
+ *
+ * This is to prevent negative numbers to be always encoded in 10 bytes.
+ * However, this is actually not needed since the usual signed LEB128
+ * always encodes with the same number of bytes as zigzag encoding
+ * following unsigned LEB128 encoding.  However, we implement zigzag
+ * since it's in wide-spread use and related to LEB128.
+ *
+ * BTW, for encoding the Protobuf documentation provides the expression
+ *
+ *     (n << 1) ^ (n >> 63)
+ *
+ * which looks nice but has undefined and implementation-defined behavior.
+ */
+static unsigned_int zigzag(signed_int si)
+{
+    unsigned_int ui = si;
+    return (ui << 1) ^ -(ui >> (uint_bits - 1));
+}
+
+static signed_int de_zigzag(unsigned_int ui)
+{
+    return (ui >> 1) ^ -(ui & 1);
 }
 
 static void unsigned_int_to_leb128(unsigned_int val, unsigned char *s)
